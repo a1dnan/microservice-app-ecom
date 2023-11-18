0e2a470c0ddb72ede0f5942362cfea10c9f994ad
a1dnan <aaliladnane@gmail.com>
2023-11-18 21:48:20 +0100

    add config service

---
+++ config-service/src/test/java/com/a1dnan/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a1dnan.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
